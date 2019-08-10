dedc507a987dfd6b52c4d6969630c3e6c6694a08
Nandanammunje <nandannayak15@gmail.com>
2019-08-10 23:43:26 +0530

    comitted
    Signed-off-by: Nandanammunje <nandannayak15@gmail.com>

--- rotate2Dby90.cpp
+++ rotate2Dby90.cpp
@@ -5,13 +5,15 @@ int main()
 int m;
 cout<<"\n Enter the size of the nxn matrix ";
 cin>>m;
-int a[m][m];
+int size=0;
+int a[m][m],b[2*m];
 cout<<"\n Enter the data into the array ";
 for(int i=0;i<m;i++)
 {
     for(int j=0;j<m;j++)
     {
         cin>>a[i][j];
+        b[size++];
     }
 }
 
