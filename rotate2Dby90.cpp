2be3991f2860a09333b6334d9c06486ba49bfa39
Nandanammunje <nandannayak15@gmail.com>
2019-08-11 14:08:43 +0530

    codecompleted
    Signed-off-by: Nandanammunje <nandannayak15@gmail.com>

--- rotate2Dby90.cpp
+++ rotate2Dby90.cpp
@@ -21,14 +21,15 @@ int temp=0;
 for(int i=0;i<m;i++)
 {
     
-    for(int j=0;j<m;j++)
+    for(int j=i+1;j<m;j++)
     {
           temp=a[i][j];
-       a[i][j]=temp;
+       a[i][j]=a[j][i];
+       a[j][i]=temp;
         
     }
 }
-/*for(int i=0;i<m;i++)
+for(int i=0;i<m;i++)
 {
     for(int j=0;j<m/2;j++)
     {
@@ -36,7 +37,7 @@ for(int i=0;i<m;i++)
         a[i][j]=a[i][m-1-j];
         a[i][m-1-j]=temp;
     }
-}*/
+}
 cout<<"\n Rotated 90 degree array is ";
 for(int i=0;i<m;i++)
 {
