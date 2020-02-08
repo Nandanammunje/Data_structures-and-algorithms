8d1c6962f0a0d9172e409a7388c0997cc7928c3d
Nandan Nayak <nandannayak15@gmail.com>
2020-02-08 18:44:52 +0530

    hashing with seperate chaining

---
+++ seperatechaining.cpp
@@ -0,0 +1,83 @@
+#include<iostream>
+#include <list>
+using namespace std;
+class Hash
+{
+int Bucket;
+list<int> *table;
+
+public:
+Hash(int V);
+void insert(int x);
+void deleteItem(int item);
+int hashfunction(int x)
+{
+    return x%Bucket;
+}
+void displayHash();
+};
+Hash::Hash(int b)
+{
+this->Bucket=b;
+table=new list<int>[Bucket];
+}
+ void Hash::insert(int x)
+ {
+   int index=hashfunction(x);
+    table[index].push_back(x);
+
+ }
+
+void Hash::deleteItem(int key)
+{
+int index=hashfunction(key);
+list<int> :: iterator i;
+for(i=table[index].begin;i!=table[index].end;i++)
+{
+if(*i == key)
+{
+    break;
+}
+if(i!=table[index].end())
+{
+    table[index].erase(i);
+}
+}
+}
+void Hash::displayHash()
+{
+    for(int i=0;i<Bucket;i++)
+    {
+        cout<<i;
+        for(auto x:table[i])
+        {
+            cout<<"-->"<<x;
+            cout<<endl;
+        }
+    }
+}
+int main()
+{
+int m,delno;
+cout<<"\n enter the size of the array ";
+cin>>m;
+int a[m];
+cout<<"\n Enter the  data into the array ";
+for(int i=0;i<m;i++)
+{
+cin>>a[i];
+}
+Hash h(m+2);
+for(int i=0;i<m;i++)
+h.insert(a[i]);
+
+cout<<"\n Enter the element to delete";
+cin>>delno;
+h.deleteItem(delno);
+
+
+
+
+
+    return 0;
+}
